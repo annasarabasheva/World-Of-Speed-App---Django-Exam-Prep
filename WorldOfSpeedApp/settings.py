e17bd46ed89eba1122f0a8772c42548779cd6d8c
Anna Sarabasheva <annasarabasheva@gmail.com>
2024-10-26 13:50:05 +0300

    Add static resources to project and configure PostgreSQL as database

--- WorldOfSpeedApp/settings.py
+++ WorldOfSpeedApp/settings.py
@@ -75,9 +75,13 @@ WSGI_APPLICATION = 'WorldOfSpeedApp.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "WorldOfSpeedApp",
+        "USER": "postgres",
+        "PASSWORD": "password",
+        "HOST": "127.0.0.1",
+        "PORT": "5432",
     }
 }
 
@@ -118,6 +122,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATICFILES_DIRS = (
+    BASE_DIR / 'static',
+)
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
